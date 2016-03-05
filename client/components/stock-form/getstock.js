fb047f1cd92c886c996f2208898f1cae0e623df9
Malika Shahrawat <malikas@mit.edu>
2016-03-05 14:25:20 -0500

    form for sectors

---
+++ client/components/stock-form/getstock.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	var sectors = {};
+	sectors["basic materials"] = ["XOM", "CVX"];
+	sectors["conglomerates"] = [];
+	sectors["consumer goods"] =["PG"];
+	sectors["financials"] = ["WFC", "HDB", "JPM", "GGAL", "BMA", "USB", "BFR", "CMA", "BAC". "BSAC"];
+	sectors["healthcare"] = ["UNH", "BMY", "VHT", "XLV", "THC", "BSX", "MNK", "JNJ", "MDT", "MRK"];
+	sectors["industrial goods"] = [];
+	sectors["services"] = [];
+	sectors["technology"] =["ACN", "CTSH", "HCKT", "BAH", "INFN", "BIDU", "GOOG", "GOOGL", "MSFT", "AAPL"];
+	sectors["utilities"] = [];
+
+
+    $("#tech").click(function() {
+    	// show data for technology stocks
+    	sectors["technology"];
+    });
+    $("#fin").click(function() {
+    	sectors["financials"];
+    });
+    $("#healthcare").click(function() {
+    	sectors["healthcare"];
+    });
+  });
